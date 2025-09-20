1bfcf90f5929b564d45a8b17bbb23fa8d96d8b53
WyattProsperie <wyattp2@umbc.edu>
2025-09-20 17:00:06 -0400

    Add files via upload

---
+++ bridge.py
@@ -0,0 +1,30 @@
+# HEY NO PEEKING
+
+name = input("Are you Bridget or Wyatt? ").strip().lower()
+
+if name == "wyatt":
+    print("Congrats on loving your girlfriend more!")
+elif name == "bridget":
+    love = input("Do you love Wyatt more? (yes/no) ").strip().lower()
+    if love == "yes":
+        print("No you don't!!!")
+    else:
+        print("You right but also why you say that")
+else:
+    print("Are you suuuuuuuuuure you spelled that right?")
+
+love_num = int(input("Give a numerical value of how much you love him: "))
+print(f"That's crazy, he loves you AT LEAST {love_num + 1}...")
+print(f"in fact, he loves you over {love_num * 2}!")
+
+relationship_num = int(input("How many months have you been together? "))
+if relationship_num < 16:
+    print("MOOOOOOOOOORE")
+elif relationship_num > 16:
+    print("Lessssssssssss (but more soon)")
+else:
+    print("Perfect amount of time!!! (but also we need more)")
+
+print("Wait does this count as typing a paragraph?")
+print("Anyways I love youuuuuu, and enjoy testing out all the different possibilities of this code!")
+print("Ily 477710 <3")
